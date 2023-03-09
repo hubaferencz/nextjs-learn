88b44bade959efe1b9abc90e4ffe5bd4b445e35c
Huba Ferencz <huba.ferencz.personal@gmail.com>
2023-03-09 10:23:38 +0200

    restart

--- medium-clone/pages/admin/[slug].js
+++
@@ -1,6 +0,0 @@
-export default function AdminPostEdit({}) {
-    return (<main>
-        <h1>Edit Post</h1>
-    </main>);
-  }
-  
--- medium-clone/pages/admin/index.js
+++
@@ -1,5 +0,0 @@
-export default function AdminPostsPage({}) {
-  return (<main>
-        <h1>Admin Posts</h1>
-  </main>);
-}
